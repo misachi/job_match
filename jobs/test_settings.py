733a83e64cf3c0aff2c6ce11afc2610362c32c9c
misach <misachibrian@gmail.com>
2018-08-28 14:22:52 +0000

    Update test settings

--- jobs/test_settings.py
+++ jobs/test_settings.py
@@ -22,4 +22,4 @@ DATABASES = {
 #        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
 #        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
 #    }
-}
+#}
